31648bde1f0d4a63fc9056f0b5998efba5a73a18
Jason Chee <jchee1@uchicago.edu>
2020-08-08 22:52:16 -0400

    can switch camera view and added oncoming vehicle

--- Prototype-1/Assets/Scripts/PlayerController.cs
+++ Prototype-1/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     private float speed = 22.0f;
     private float turnSpeed = 45.0f;
+
+    //camera switcher variables
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,5 +28,12 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         //turn vehicle
         transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
+
+        //if input then switch camera
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
